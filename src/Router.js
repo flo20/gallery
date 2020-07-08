11dec51a8511119a31735c8efec68080c0546e18
flo20 <59807384+flo20@users.noreply.github.com>
2020-07-08 08:26:58 +0300

    Recovered docs

--- src/Router.js
+++ src/Router.js
@@ -4,14 +4,13 @@ import App from "./App";
 import ImageView from "./components/ImageView/ImageView";
 import ImageUpload from "./components/ImageUpload/ImageUpload";
 import ResultDisplayed from "./components/ResultDisplayed/ResultDisplayed";
-import ImageList from "./components/ImageList/ImageList";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={App} />
-        <Route path="/image/:id" component={ImageList} />
+
         <Route path="/image/:id" component={ImageView} />
         <Route path="/upload" component={ImageUpload} />
         <Route path="/myGallery" component={ResultDisplayed} />
