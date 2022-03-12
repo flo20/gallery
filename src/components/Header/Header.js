a94c2c454a4b849bf304466fc98667788a7e3d3d
Florence Anipa <mawuaniipa@gmail.com>
2022-03-12 23:02:06 +0200

    Add hamburger menu/ styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import { Link } from "react-router-dom";
+import {GiHamburgerMenu} from "react-icons/gi"
+import {GrClose} from "react-icons/gr"
+
+
 import "./Header.css";
 
 const Header = () => {
+  const [openHamburger,setOpenHamburger] = useState(false)
+ // const [isMobileView, setIsMobileView] = useState(false)
+
+  const handleToggle = () =>{
+   setOpenHamburger(!openHamburger)
+   //setIsMobileView(true)
+  }
+
+
   return (
-    <>
+    <Fragment>
       <div className="wrap">
+        <div>
         <Link to="/" id="logo">
           GALLERY
         </Link>
-        <div className="head">
-          <Link to="/upload" id="links">
+        </div>
+
+        <div className={openHamburger ? "head active" : "head"}>
+          <div className="navMenu">
+          <Link to="/upload" className="links">
             Upload Image
           </Link>
-          <Link to="/myGallery" id="links">
+          </div>
+        <div className="navMenu">
+        <Link to="/myGallery" className="links">
             MyGallery
           </Link>
+        </div>  
         </div>
+{openHamburger ? <GrClose className="hamburgerMenu" onClick={handleToggle}/> : <GiHamburgerMenu className="hamburgerMenu" onClick={handleToggle}/> }
       </div>
-    </>
+    </Fragment>
   );
 };
 
